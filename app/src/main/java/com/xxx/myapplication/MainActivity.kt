2c7ea6f9516ffc45ec70dd59082deca7792f12e9
jiangshuai321 <838318633@qq.com>
2021-09-17 11:06:00 +0800

    1.2

--- app/src/main/java/com/xxx/myapplication/MainActivity.kt
+++ app/src/main/java/com/xxx/myapplication/MainActivity.kt
@@ -2,10 +2,12 @@ package com.xxx.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val flContainer = findViewById<FrameLayout>(R.id.flContainer)
     }
 }
